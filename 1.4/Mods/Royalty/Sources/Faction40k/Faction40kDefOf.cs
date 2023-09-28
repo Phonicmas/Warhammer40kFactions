f67391d709e03abab17b2c052103373689a8584f
Phonicmas <jkskovlund@gmail.com>
2023-09-28 14:57:24 +0200

    Patch 28/9

--- 1.4/Mods/Royalty/Sources/Faction40k/Faction40kDefOf.cs
+++ 1.4/Mods/Royalty/Sources/Faction40k/Faction40kDefOf.cs
@@ -7,30 +7,6 @@ namespace Faction40k
     [DefOf]
     public static class Faction40kDefOf
     {
-        public static GeneDef BEWH_SecondaryHeart;
-        public static GeneDef BEWH_Ossmodula;
-        public static GeneDef BEWH_Biscopea;
-        public static GeneDef BEWH_Haemastamen;
-        public static GeneDef BEWH_LarramansOrgan;
-        public static GeneDef BEWH_CatalepseanNode;
-        public static GeneDef BEWH_Preomnor;
-        public static GeneDef BEWH_Omophagea;
-        public static GeneDef BEWH_MultiLung;
-        public static GeneDef BEWH_Occulobe;
-        public static GeneDef BEWH_LymansEar;
-        public static GeneDef BEWH_SusAnMembrane;
-        public static GeneDef BEWH_Melanochrome;
-        public static GeneDef BEWH_OoliticKidney;
-        public static GeneDef BEWH_Neuroglottis;
-        public static GeneDef BEWH_Mucranoid;
-        public static GeneDef BEWH_BetchersGland;
-        public static GeneDef BEWH_ProgenoidGlands;
-        public static GeneDef BEWH_BlackCarapace;
-
-        public static GeneDef BEWH_SinewCoil;
-        public static GeneDef BEWH_Magnificat;
-        public static GeneDef BEWH_BelisarianFurnace;
-
         static Faction40kDefOf()
         {
             DefOfHelper.EnsureInitializedInCtor(typeof(Faction40kDefOf));
